2be3eabfc86bbe2a9a53ee2bcfaa0cdc90b5511e
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2021-10-04 23:39:27 +0300

    next commit

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -3,11 +3,11 @@ import cl from './NavBar.module.css'
 const NavBar = () =>{
     return (
         <div className={cl.nav_bar} >
-            <div className={cl.navLink}><NavLink to="/Profile">Profile</NavLink></div>
-            <div className={cl.navLink}><NavLink to="/Dialogs">Messages</NavLink></div>
-            <div className={cl.navLink}><NavLink to="/News">News</NavLink></div>
-            <div className={cl.navLink}><NavLink to="/Music">Music</NavLink></div>
-            <div className={cl.navLink}><NavLink to="/Setting">Setting</NavLink></div>
+            <div className={cl.navLink}><NavLink to="/Profile" className={cl.mamas} activeClassName={cl.active_link}>Profile</NavLink></div>
+            <div className={cl.navLink}><NavLink to="/Dialogs" activeClassName={cl.active_link}>Messages</NavLink></div>
+            <div className={cl.navLink}><NavLink to="/News" activeClassName={cl.active_link}>News</NavLink></div>
+            <div className={cl.navLink}><NavLink to="/Music" activeClassName={cl.active_link}>Music</NavLink></div>
+            <div className={cl.navLink}><NavLink to="/Setting" activeClassName={cl.active_link}>Setting</NavLink></div>
         </div>
     )
 }
